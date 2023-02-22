417ac5bab79ac428de8cbf0728d2b9f22e0b978e
satyabratait <satyabrata@itobuz.com>
2023-02-22 17:42:07 +0530

    fourth commit

--- javascript/working.js
+++ javascript/working.js
@@ -1,16 +1,16 @@
-// import Swiper from "swiper";
-// const Swiper = require('Swiper')
+const emailform = document.getElementById("inputemailforms"); //to take input of email from section 14
+const submit = document.getElementById("submitbutton");
+const input = document.getElementById("inputname"); //to take input of name from section 14
+const weburl = document.getElementById("validateweburl"); //to take input of weburl
+const submitbutton = document.getElementById("submitbuttons"); 
+const emailcontrol = document.getElementById("emailcontrol"); //to take input from section 10
+const searchbar = document.querySelector(".search-bar");
+const searchbutton = document.querySelector(".search-submit");
 
-let emailform = document.getElementById("inputemailforms");
-let submit = document.getElementById("submitbutton");
-let input = document.getElementById("inputname");
-let weburl = document.getElementById("validateweburl");
-let submitbutton = document.getElementById("submitbuttons");
-let emailcontrol = document.getElementById("emailcontrol");
-let price = document.querySelectorAll(".chooseprice");
+const reges = /^[a-zA-Z0-9]+([\.-]?[a-zA-Z0-9]+)*@[a-zA-Z]+([\.-]?[a-zA-Z0-9]+)*(\.[a-zA-Z0-9]{2,3})+$/g; //expression to check email
+const namereges = /^[a-zA-Z]$/; //expression to check name
 
-let reges = /^[a-zA-Z0-9]+([\.-]?[a-zA-Z0-9]+)*@[a-zA-Z]+([\.-]?[a-zA-Z0-9]+)*(\.[a-zA-Z0-9]{2,3})+$/g;
-let namereges = /^[a-zA-Z]$/;
+//submitbutton of section 14
 submit.addEventListener("click",(e) => {
     if (!namereges.test(input.value)) {
                 alert("enter proper name");
@@ -20,12 +20,14 @@ submit.addEventListener("click",(e) => {
     testemail(emailform.value);
 });
 
+// to check email
 function testemail(value) {
     if (!reges.test(value)) {
         alert("Invalid email");
     }
 }
 
+//submitbutton of section 10
 submitbutton.addEventListener("click",(e) =>{
     let givenurl;
     testemail(emailcontrol.value);
@@ -38,6 +40,7 @@ submitbutton.addEventListener("click",(e) =>{
     }
 });
 
+// slider of section 13
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
     spaceBetween: 20,
@@ -59,28 +62,12 @@ var swiper = new Swiper(".mySwiper", {
     }
 });
 
-
-price.forEach((card) => {
-    card.addEventListener("mouseover", () => {
-        card.firstElementChild.style.color = "white";
-    });
+//searchbutton
+searchbutton.addEventListener("click", ()=>{
+    if (searchbar.classList.contains('hide')) {
+        searchbar.classList.remove('hide');
+    }
+    else{
+        searchbar.classList.add('hide');
+    }
 });
-
-price.forEach((card) => {
-    card.addEventListener("mouseout", () => {
-      card.firstElementChild.style.color = "#6f77f1"
-    })
-});
-
-price.forEach((button) => {
-    button.addEventListener("mouseover", () => {
-        console.log(button.lastElementChild);
-      button.lastElementChild.lastElementChild.style.backgroundColor = "#FAD725"
-    })
-  })
-  
-price.forEach((button) => {
-    button.addEventListener("mouseout", () => {
-      button.lastElementChild.lastElementChild.style.backgroundColor = "white"
-    })
-})
